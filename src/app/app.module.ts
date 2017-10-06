5c7def384db35b00e48a4124e422f0a56d2882e0
Haoming Yin <hyi25@uclive.ac.nz>
2017-10-06 22:29:37 +1300

    Added nav bar and main page carousel.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
