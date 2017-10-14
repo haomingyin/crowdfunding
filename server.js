17a64a2a12bcf93344117234ba77062895b913db
Haoming Yin <hyi25@uclive.ac.nz>
2017-10-14 15:33:49 +1300

    Fixed server.js so that users can manually type url

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.all('*', express.static(__dirname + '/dist'));
+app.use('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
